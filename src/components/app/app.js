e9370e147c930c3c47760f62708907b7e6d395c7
Kirill Marenov <kmarenov@gmail.com>
2020-02-28 12:22:26 +0300

    134. Роутинг

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './app.css';
-import { withBookstoreService } from '../hoc';
+import { HomePage, CartPage } from '../pages'
+import { Route, Switch } from 'react-router-dom';
 
-const App = ({ bookstoreService }) => {
-    console.log(bookstoreService.getBooks());
-    return <div>App</div>;
+const App = () => {
+    return (
+        <Switch>
+            <Route
+                path="/"
+                component={HomePage}
+                exact />
+            <Route
+                path="/cart"
+                component={CartPage}
+                />
+        </Switch>
+    );
 };
 
-export default withBookstoreService()(App);
+export default App;
---
+++ src/components/pages/home-page.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BookList from "../book-list";
+
+const HomePage = () => {
+    return (
+        <BookList books={[]} />
+    );
+};
+
+export default HomePage;
