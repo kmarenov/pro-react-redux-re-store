fc650305eb8f42eb25e00f3bb0530f769a8963f1
Kirill Marenov <kmarenov@gmail.com>
2020-02-28 15:52:40 +0300

    137. Стили (опциональное видео)

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import './app.css';
 import { HomePage, CartPage } from '../pages'
+import ShopHeader from '../shop-header';
+import ShoppingCartTable from '../shopping-cart-table';
 import { Route, Switch } from 'react-router-dom';
 
 const App = () => {
     return (
-        <Switch>
-            <Route
-                path="/"
-                component={HomePage}
-                exact />
-            <Route
-                path="/cart"
-                component={CartPage}
-                />
-        </Switch>
+        <main role="main" className="container">
+            <ShopHeader numItems={5} total={210} />
+            <Switch>
+                <Route
+                    path="/"
+                    component={HomePage}
+                    exact />
+                <Route
+                    path="/cart"
+                    component={CartPage}
+                    />
+            </Switch>
+            <ShoppingCartTable />
+        </main>
     );
 };
 
--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -16,7 +16,7 @@ class BookList extends Component {
     render() {
         const { books } = this.props;
         return (
-            <ul>
+            <ul className="book-list">
                 {
                     books.map((book) => {
                         return (
