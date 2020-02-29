5198885d26761872574d51dd06edbc689710539c
Kirill Marenov <kmarenov@gmail.com>
2020-02-29 15:10:32 +0300

    141. mapDispatchToProps: аргумент ownProps

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -10,12 +10,7 @@ import ErrorIndicator from '../error-indicator';
 
 class BookList extends Component {
     componentDidMount() {
-        const { bookstoreService, booksLoaded, booksRequested, booksError } = this.props;
-        booksRequested();
-        bookstoreService.getBooks()
-            .then((data) => booksLoaded(data))
-            .catch((err) => booksError(err))
-        ;
+        this.props.fetchBooks();
     }
 
     render() {
@@ -47,7 +42,18 @@ const mapStateToProps = ({ books, loading, error }) => {
     return { books, loading, error };
 };
 
-const mapDispatchToProps = { booksLoaded, booksRequested, booksError };
+const mapDispatchToProps = (dispatch, ownProps) => {
+    const { bookstoreService } = ownProps;
+    return {
+        fetchBooks: () => {
+            dispatch(booksRequested());
+            bookstoreService.getBooks()
+                .then((data) => dispatch(booksLoaded(data)))
+                .catch((err) => dispatch(booksError(err)))
+            ;
+        }
+    }
+};
 
 export default compose(
     withBookstoreService(),
