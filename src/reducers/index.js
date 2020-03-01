3b8c3a8872e666cebeb86abade2cb26e71058699
Kirill Marenov <kmarenov@gmail.com>
2020-03-01 21:31:02 +0300

    144. Подключение нового компонента к Redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,29 @@
 const initialState = {
     books: [],
     loading: true,
-    error: null
+    error: null,
+    cartItems: [
+        {
+            id: 1,
+            name: 'Book 1',
+            count: 3,
+            total: 150
+        },
+        {
+            id: 2,
+            name: 'Book 2',
+            count: 2,
+            total: 70
+        }
+    ],
+    orderTotal: 590
 };
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'FETCH_BOOKS_REQUEST':
             return {
+                ...state,
                 books: [],
                 loading: true,
                 error: null
@@ -15,6 +31,7 @@ const reducer = (state = initialState, action) => {
 
         case 'FETCH_BOOKS_SUCCESS':
             return {
+                ...state,
                 books: action.payload,
                 loading: false,
                 error: null
@@ -22,6 +39,7 @@ const reducer = (state = initialState, action) => {
 
         case 'FETCH_BOOKS_FAILURE':
             return {
+                ...state,
                 books: [],
                 loading: false,
                 error: action.payload
